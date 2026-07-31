2eac6f94e7e4d5d90e30e1411eb226d092925443
AigerimBeisembekova <aig.beisembekova@gmail.com>
2026-07-31 17:22:25 +0500

    set prettier

--- main.js
+++ main.js
@@ -1,62 +1,57 @@
-const firstCard = document.querySelector('.products__item');
-const changeFirstCardBtn = document.querySelector('#change-first-card-btn');
+const firstCard = document.querySelector(".products__item");
+const changeFirstCardBtn = document.querySelector("#change-first-card-btn");
 
-changeFirstCardBtn.addEventListener('click', () => {
-    firstCard.style.backgroundColor = '#e8f5e9'; // Легкий зеленый
+changeFirstCardBtn.addEventListener("click", () => {
+  firstCard.style.backgroundColor = "#e8f5e9"; // Легкий зеленый
 });
 
+const allCards = document.querySelectorAll(".products__item");
+const changeAllCardsBtn = document.querySelector("#change-all-cards-btn");
 
-const allCards = document.querySelectorAll('.products__item');
-const changeAllCardsBtn = document.querySelector('#change-all-cards-btn');
-
-changeAllCardsBtn.addEventListener('click', () => {
-    const greenColorHash = '#00FF00';
-    allCards.forEach((card) => {
-        card.style.backgroundColor = greenColorHash;
-    });
+changeAllCardsBtn.addEventListener("click", () => {
+  const greenColorHash = "#00FF00";
+  allCards.forEach((card) => {
+    card.style.backgroundColor = greenColorHash;
+  });
 });
 
+const openGoogleBtn = document.querySelector("#open-google-btn");
 
-const openGoogleBtn = document.querySelector('#open-google-btn');
-
-openGoogleBtn.addEventListener('click', openGoogleInNewTab);
+openGoogleBtn.addEventListener("click", openGoogleInNewTab);
 
 function openGoogleInNewTab() {
-    const newTab = confirm("Вы хотите открыть Google в новой вкладке?");
+  const newTab = confirm("Вы хотите открыть Google в новой вкладке?");
 
-    if (newTab === true) {
-        window.open('https://www.google.com');
-    }
+  if (newTab === true) {
+    window.open("https://www.google.com");
+  }
 }
 
+const showAlertBtn = document.querySelector("#show-alert-btn");
 
-const showAlertBtn = document.querySelector('#show-alert-btn');
-
-showAlertBtn.addEventListener('click', function() {
-    logAndAlert('ДЗ №6');
+showAlertBtn.addEventListener("click", function () {
+  logAndAlert("ДЗ №6");
 });
 
 function logAndAlert(message) {
-    console.log(message);
-    alert(message);
+  console.log(message);
+  alert(message);
 }
 
+const mainTitle = document.querySelector(".title.catalog__title");
 
-const mainTitle = document.querySelector('.title.catalog__title');
-
-mainTitle.addEventListener('mouseover', function() {
-    logElementText(mainTitle);
+mainTitle.addEventListener("mouseover", function () {
+  logElementText(mainTitle);
 });
 
 function logElementText(element) {
-    console.log(element.textContent.trim());
+  console.log(element.textContent.trim());
 }
 
+const toggleColorBtn = document.querySelector("#toggle-color-btn");
 
-const toggleColorBtn = document.querySelector('#toggle-color-btn');
-
-toggleColorBtn.addEventListener('click', () => {
+toggleColorBtn.addEventListener("click", () => {
   // Нажали 1 раз — добавился 'purple-state' (кнопка фиолетовая)
   // Нажали 2 раз — удалился 'purple-state' (кнопка снова зелёная)
-  toggleColorBtn.classList.toggle('purple-state');
+  toggleColorBtn.classList.toggle("purple-state");
 });
